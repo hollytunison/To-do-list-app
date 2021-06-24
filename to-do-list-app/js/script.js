9ecb10f074961c1e8fa9204f8ac8def6f9c04300
Holly Tunison <84026433+hollytunison@users.noreply.github.com>
2021-06-23 21:41:26 -0600

    fixed "js/script.js" in html file

--- to-do-list-app/js/script.js
+++ to-do-list-app/js/script.js
@@ -1,34 +1,40 @@
 function newItem() {
 
-
-    //Adding a new item to the list:
+    //javascript
+    //1. Adding a new item to the list of items:
     let li = $('<li></li>');
     let inputValue = $('#input').val();
-    li.append(inputValue);
 
+    li.append(inputValue);
     if (inputValue === '') {
         alert("You must write something!");
     } else {
-        $('#list').append(li);
+        let list = $('#list');
+        list.append(li);
     }
 
-    //Crossing an item out:
+    //2. Crossing out an item from the list of items:
     li.on('dblclick', function crossOut() {
-        li.toggleClass('strike');
-    })
+            li.toggleClass('strike');
+        })
+        //  function crossOut() {
+        // 	li.classList.toggle("strike");
+        // }
 
-    //Adding a delete button "X":
-    let crossOutButton = $("<crossOutButton></crossOutButton>");
+    // li.addEventListener("dblclick",crossOut);
+
+    //3(i). Adding the delete button "X":
+
+    let crossOutButton = $('<crossOutButton></crossOutButton>');
     crossOutButton.append(document.createTextNode("X"));
     li.append(crossOutButton);
 
     crossOutButton.on("click", deleteListItem);
-
-    //Adding class delete (Display: NONE) from CSS:
+    //3(ii). Adding CLASS DELETE (DISPLAY: NONE) from the css:
     function deleteListItem() {
-        li.addClass('delete')
+        li.addClass("delete")
     }
-
-    //Reordering items:
+    // 4. Reordering the items:
     $('#list').sortable();
+
 }
